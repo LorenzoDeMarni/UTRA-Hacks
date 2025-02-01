b70be5b486b12d5d93008611f8af177b937af29d
Vincent <vinhvincent.ngu@gmail.com>
2025-02-01 15:43:37 -0500

    robot movements

--- challenge1/challenge1.ino
+++ challenge1/challenge1.ino
@@ -35,29 +35,80 @@ void color_setup() {
     Serial.begin(9600);
 }
 
-void motor_setup() {
+void setup() {
+    // Set motor control pins as outputs
+    pinMode(EN_A, OUTPUT);
     pinMode(motor1Pin1, OUTPUT);
     pinMode(motor1Pin2, OUTPUT);
     pinMode(motor2Pin1, OUTPUT);
     pinMode(motor2Pin2, OUTPUT);
+    pinMode(EN_B, OUTPUT);
+
     Serial.begin(9600);
+    Serial.println("Motor System Initialized");
+    
 }
 
-void base_move() {
-    if (Serial.available() > 0) {
-        char command = Serial.read();
-        if (command == 'F') { // Move Forward
-            digitalWrite(motor1Pin1, HIGH);
-            digitalWrite(motor1Pin2, LOW);
-            digitalWrite(motor2Pin1, HIGH);
-            digitalWrite(motor2Pin2, LOW);
-        } else if (command == 'S') { // Stop
-            digitalWrite(motor1Pin1, LOW);
-            digitalWrite(motor1Pin2, LOW);
-            digitalWrite(motor2Pin1, LOW);
-            digitalWrite(motor2Pin2, LOW);
-        }
-    }
+void loop() {
+    // Move forward for 2 seconds
+    moveForward(2000);
+    // Turn left for 1 second
+    turnLeft(1000);
+    // Move forward for 2 seconds
+    moveForward(2000);
+    // Turn right for 1 second
+    turnRight(1000);
+    // Move forward for 2 seconds
+    moveForward(2000);
+    // Stop motors for 1 second
+    stopMotors();
+    delay(1000);
+    break;
+
+}
+
+void moveForward(int duration) {
+    Serial.println("Moving Forward...");
+    analogWrite(EN_A, motorSpeed); // Set speed
+    analogWrite(EN_B, motorSpeed);
+    digitalWrite(motor1Pin1, HIGH);
+    digitalWrite(motor1Pin2, LOW);
+    digitalWrite(motor2Pin1, HIGH);
+    digitalWrite(motor2Pin2, LOW);
+    delay(duration);
+    stopMotors();
+}
+
+void turnLeft(int duration) {
+    Serial.println("Turning Left...");
+    analogWrite(EN_A, motorSpeed); // Set speed
+    analogWrite(EN_B, motorSpeed);
+    digitalWrite(motor1Pin1, LOW);
+    digitalWrite(motor1Pin2, HIGH);
+    digitalWrite(motor2Pin1, HIGH);
+    digitalWrite(motor2Pin2, LOW);
+    delay(duration);
+    stopMotors();
+}
+
+void turnRight(int duration) {
+    Serial.println("Turning Right...");
+    analogWrite(EN_A, motorSpeed); // Set speed
+    analogWrite(EN_B, motorSpeed);
+    digitalWrite(motor1Pin1, HIGH);
+    digitalWrite(motor1Pin2, LOW);
+    digitalWrite(motor2Pin1, LOW);
+    digitalWrite(motor2Pin2, HIGH);
+    delay(duration);
+    stopMotors();
+}
+
+void stopMotors() {
+    Serial.println("Stopping Motors...");
+    digitalWrite(motor1Pin1, LOW);
+    digitalWrite(motor1Pin2, LOW);
+    digitalWrite(motor2Pin1, LOW);
+    digitalWrite(motor2Pin2, LOW);
 }
 
 String get_color() {
