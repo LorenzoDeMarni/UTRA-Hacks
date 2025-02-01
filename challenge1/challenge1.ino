1bbb545d6a4ee74cee89c3a954f959650165ab5f
Lorenzo DeMarni <lorenzodemarni666@gmail.com>
2025-02-01 15:03:11 -0500

    Steps to complete challenge with colour detection functions

--- challenge1/challenge1.ino
+++ challenge1/challenge1.ino
@@ -1,7 +1,22 @@
-const int motor1Pin1 = 9;  // Motor 1 forward
-const int motor1Pin2 = 10; // Motor 1 backward
-const int motor2Pin1 = 5;  // Motor 2 forward
-const int motor2Pin2 = 6;  // Motor 2 backward
+const int EN_A = 11; //Enable pin for first motor
+const int IN1 = 9; //control pin for first motor
+const int IN2 = 8; //control pin for first motor
+const int IN3 = 7; //control pin for second motor
+const int IN4 = 6; //control pin for second motor
+const int EN_B = 10; //Enable pin for second motor
+# https://www.instructables.com/Running-DC-Motor-With-Arduino-Using-L298N-Motor-Dr/
+// change the pin values for light sensor
+
+#define S0 4;
+#define S1 5;
+#define S2 6;
+#define S3 7;
+#define sensorOut 8;
+#define red 0;
+#define green 0;
+#define blue 0;
+
+
 
 void setup() {
     pinMode(motor1Pin1, OUTPUT);
@@ -9,6 +24,8 @@ void setup() {
     pinMode(motor2Pin1, OUTPUT);
     pinMode(motor2Pin2, OUTPUT);
     Serial.begin(9600);
+    
+
 }
 
 void loop() {
@@ -26,4 +43,77 @@ void loop() {
             digitalWrite(motor2Pin2, LOW);
         }
     }
+    
+    // figure out where first colour is and start timer
+    // keep going till hit black and stop timer
+    // 180* turn, go halfway
+    // 90* turn right and go straight till hit black
+    // 180* turn , go till hit black
+    // 180* turn, and go halfway
+    // plant flag including offset
 }
+
+void color_setup() {
+    pinMode(S0, OUTPUT);
+    pinMode(S1, OUTPUT);
+    digitalWrite(S0, HIGH);
+    digitalWrite(S1, LOW);
+
+    pinMode(S2, OUTPUT);
+    pinMode(S3, OUTPUT);
+    pinMode(sensorOut, INPUT);
+
+    Serial.begin(9600);
+}
+
+String get_color() {
+
+    digitalWrite(S2, LOW);
+    digitalWrite(S3, LOW);
+    red = pulseIn(sensorOut, LOW);
+
+    // Read Green
+    digitalWrite(S2, HIGH);
+    digitalWrite(S3, HIGH);
+    green = pulseIn(sensorOut, LOW);
+
+    // Read Blue
+    digitalWrite(S2, LOW);
+    digitalWrite(S3, HIGH);
+    blue = pulseIn(sensorOut, LOW);
+
+    Serial.print("Red: ");
+    Serial.print(red);
+    Serial.print("  Green: ");
+    Serial.print(green);
+    Serial.print("  Blue: ");
+    Serial.println(blue);
+
+    String color = identifyColor(red, green, blue);
+    return color;
+
+    delay(200);
+
+}
+
+String identifyColor(int r, int g, int b) {
+    if (r < g && r < b) {
+        return "RED";
+    } 
+    else if (g < r && g < b) {
+        return "GREEN";
+    } 
+    else if (b < r && b < g) {
+        return "BLUE";
+    } 
+    else if (r > 200 && g > 200 && b > 200) {
+        return "WHITE";
+    } 
+    else if (r < 50 && g < 50 && b < 50) {
+        return "BLACK";
+    } 
+    else {
+        return "UNKNOWN";
+    }
+
+
