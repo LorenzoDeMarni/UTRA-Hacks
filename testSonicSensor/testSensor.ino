b4f60a4dcc1f9646f6c93a44670d0829722d3129
Vincent <vinhvincent.ngu@gmail.com>
2025-02-01 20:00:55 -0500

    testing for sonic sensor

---
+++ testSonicSensor/testSensor.ino
@@ -0,0 +1,35 @@
+#define TRIG_PIN A0
+#define ECHO_PIN A1
+#define WALL_DISTANCE_THRESHOLD 100
+
+void setup() {
+    pinMode(TRIG_PIN, OUTPUT);
+    pinMode(ECHO_PIN, INPUT);
+    Serial.begin(9600);
+}
+
+void loop() {
+    long duration;
+    float distance = 0.0;
+
+    digitalWrite(TRIG_PIN, LOW);
+    delayMicroseconds(2);
+    digitalWrite(TRIG_PIN, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIG_PIN, LOW);
+
+    // Measure the time it takes for the echo to return
+    duration = pulseIn(ECHO_PIN, HIGH);
+
+    // Convert the time to distance (in mm)
+    distance = (duration * 0.343) / 2; 
+    delay(100);
+
+    if (distance > 0 && distance <= WALL_DISTANCE_THRESHOLD) {
+       Serial.println("STOP OMG OMG ABORT ABORT ABORT WERE GONNA DIE");
+       delay(500);
+    } 
+    else {
+       Serial.println("aye u good bro");
+    }
+}
