bcaa598c71a845c4b919098431206c843abfdb58
adelynntran <161761042+adelynntran@users.noreply.github.com>
2025-02-02 03:35:41 -0500

    add new color dectector file

--- challenge1/challenge1.ino
+++ challenge1/challenge1.ino
@@ -1,26 +1,41 @@
+#include <Servo.h>  // Include Servo library
+
+#define SERVO_PIN A3  // Servo connected to Analog Pin A3
+
+Servo myServo;  // Create Servo object
+
 // Motor Driver Pin Definitions
-#define EN_A 11  // PWM speed control for right
 #define motor1Pin1 6   // Control pin for second motor
 #define motor1Pin2 7   // Control pin for second motor
-//left motor
+//right motor
+
 #define motor2Pin1 8   // Control pin for first motor
 #define motor2Pin2 9   // Control pin for first motor
-//right method
+//left motor
+
 #define EN_B 10  // PWM speed control for left
+#define EN_A 11  // PWM speed control for right
 
 // Motor speed (adjustable between 0-255)
-int motorSpeed = 150;  // 150 is about 60% speed
-
-# https://www.instructables.com/Running-DC-Motor-With-Arduino-Using-L298N-Motor-Dr/
+int motorSpeedLeft = 145;  
+int motorSpeedRight = 150;
 
 // change the pin values for light sensor
 #define S0 2;
 #define S1 3;
-#define S2 4;
-#define S3 5;
-#define sensorOut 8;
+#define S2 5;
+#define S3 4;
+#define sensorOut 12;
 
 int red = 0, green = 0, blue = 0;
+
+#define turn_value 820
+
+#define longerRuntime 0
+unsigned long startTime = 0; // Stores the start time
+unsigned long stopTime = 0;  // Stores the stop time
+unsigned long elapsedTime = 0; //total elapsed time
+bool timerRunning = false;
 
 void setup() {
     // Set motor control pins as outputs
@@ -31,12 +46,7 @@ void setup() {
     pinMode(motor2Pin2, OUTPUT);
     pinMode(EN_B, OUTPUT);
 
-    Serial.begin(9600);
-    Serial.println("Motor System Initialized");
-    
-}
-
-void color_setup() {
+    //Set Color sensor pins
     pinMode(S0, OUTPUT);
     pinMode(S1, OUTPUT);
     digitalWrite(S0, HIGH);
@@ -46,86 +56,86 @@ void color_setup() {
     pinMode(S3, OUTPUT);
     pinMode(sensorOut, INPUT);
 
+    myServo.attach(SERVO_PIN);  // Attach servo to pin A3
+   
+    //calling to pick up flag at the start
+    pickUpFlag();
+    
+    startUp(); //move into the first ring...
     Serial.begin(9600);
+    
 }
 
-void motor_setup() {
-    pinMode(motor1Pin1, OUTPUT);
-    pinMode(motor1Pin2, OUTPUT);
-    pinMode(motor2Pin1, OUTPUT);
-    pinMode(motor2Pin2, OUTPUT);
-    Serial.begin(9600);
-}
+void loop() {    
+    
+    red = getColorReading(LOW, LOW); // Read Red
+    green = getColorReading(HIGH, HIGH); // Read Green
+    blue = getColorReading(LOW, HIGH); // Read Blue
 
-void loop() {
-    // Move forward for 2 seconds
-    moveForward(2000);
-    // Turn left for 1 second
-    turnLeft(1000);
-    // Move forward for 2 seconds
-    moveForward(2000);
-    // Turn right for 1 second
-    turnRight(1000);
-    // Move forward for 2 seconds
-    moveForward(2000);
-    // Stop motors for 1 second
-    stopMotors();
-    delay(1000);
-    break;
+    String colorNew = identifyColor(red, green, blue);
 
-}
-
-
-void base_move() {
-    if (Serial.available() > 0) {
-        char command = Serial.read();
-        if (command == 'F') { // Move Forward
-            digitalWrite(motor1Pin1, HIGH);
-            digitalWrite(motor1Pin2, LOW);
-            digitalWrite(motor2Pin1, HIGH);
-            digitalWrite(motor2Pin2, LOW);
-        } else if (command == 'S') { // Stop
-            digitalWrite(motor1Pin1, LOW);
-            digitalWrite(motor1Pin2, LOW);
-            digitalWrite(motor2Pin1, LOW);
-            digitalWrite(motor2Pin2, LOW);
-        }
+    while (colorNew != "BLACK") { //keeps going forward till we detect black (not a color)
+        moveForward();
+        startTime = millis();  //we wanna track the time it takes to reach start to 'black' point
+        timerRunning = true;
+        Serial.println("Timer Started!");
     }
+
+    stopTime = millis();
+    timerRunning = false;
+    elapsedTime = stopTime - startTime; //save travel time to a variable
+
+    if (elapsedTime > longerRuntime) {
+        longerRuntime = elapsedTime
+        turnLeft(); //need to make this a sharper turn with a parameter
+        loop();
+    }
+    //we want this if statement to check if the run time was longer than before. This will let us turn
+    //on a steeper angle (want to get to at least 180 90 is default)
+
+    else {
+        turnLeft();
+        turnLeft(); //do a 180
+        moveForward(); //make this parameter elapsedTime / 2 (midway through circle)
+        dropFlag();
+        while (true);
+    }
+    //when we reach desired state, we can return on the same line traveled and drop the flag half way
+    //which should be the center
+
 }
 
-void moveForward(int duration) {
+void moveForward() {
     Serial.println("Moving Forward...");
-    analogWrite(EN_A, motorSpeed); // Set speed
-    analogWrite(EN_B, motorSpeed);
+    analogWrite(EN_A, motorSpeedLeft); // Set speed
+    analogWrite(EN_B, motorSpeedRight);
     digitalWrite(motor1Pin1, HIGH);
     digitalWrite(motor1Pin2, LOW);
     digitalWrite(motor2Pin1, HIGH);
     digitalWrite(motor2Pin2, LOW);
-    delay(duration);
-    stopMotors();
 }
 
-void turnLeft(int duration) {
+void turnLeft() {
     Serial.println("Turning Left...");
-    analogWrite(EN_A, motorSpeed); // Set speed
-    analogWrite(EN_B, motorSpeed);
+    analogWrite(EN_A, motorSpeedLeft); // Set speed
+    analogWrite(EN_B, motorSpeedRight);
     digitalWrite(motor1Pin1, LOW);
     digitalWrite(motor1Pin2, HIGH);
     digitalWrite(motor2Pin1, HIGH);
     digitalWrite(motor2Pin2, LOW);
-    delay(duration);
+    delay(turn_value);
     stopMotors();
 }
 
-void turnRight(int duration) {
+void turnRight() {
     Serial.println("Turning Right...");
-    analogWrite(EN_A, motorSpeed); // Set speed
-    analogWrite(EN_B, motorSpeed);
+    analogWrite(EN_A, motorSpeedLeft); // Set speed
+    analogWrite(EN_B, motorSpeedRight);
     digitalWrite(motor1Pin1, HIGH);
     digitalWrite(motor1Pin2, LOW);
     digitalWrite(motor2Pin1, LOW);
     digitalWrite(motor2Pin2, HIGH);
-    delay(duration);
+    delay(turn_value);
     stopMotors();
 }
 
@@ -137,53 +147,59 @@ void stopMotors() {
     digitalWrite(motor2Pin2, LOW);
 }
 
-String get_color() {
-
-    // Read Red
-    digitalWrite(S2, LOW);
-    digitalWrite(S3, LOW);
-    red = pulseIn(sensorOut, LOW);
-
-    // Read Green
-    digitalWrite(S2, HIGH);
-    digitalWrite(S3, HIGH);
-    green = pulseIn(sensorOut, LOW);
-
-    // Read Blue
-    digitalWrite(S2, LOW);
-    digitalWrite(S3, HIGH);
-    blue = pulseIn(sensorOut, LOW);
-
-    Serial.print("Red: ");
-    Serial.print(red);
-    Serial.print("  Green: ");
-    Serial.print(green);
-    Serial.print("  Blue: ");
-    Serial.println(blue);
-
-    String color = identifyColor(red, green, blue);
-    return color;
-
-    delay(200);
-
+String identifyColor(int r, int g, int b) {
+    if (r < g - 15 && r < b - 15) return "RED";
+    else if (g < r - 15 && g < b - 15) return "GREEN";
+    else if (b < r - 15 && b < g - 15) return "BLUE";
+    else return "BLACK";  // Default to BLACK if not RED, GREEN, or BLUE
 }
 
-String identifyColor(int r, int g, int b) {
-    if (r < g && r < b) {
-        return "RED";
-    } 
-    else if (g < r && g < b) {
-        return "GREEN";
-    } 
-    else if (b < r && b < g) {
-        return "BLUE";
-    } 
-    else if (r > 200 && g > 200 && b > 200) {
-        return "WHITE";
-    } 
-    else if (r < 50 && g < 50 && b < 50) {
-        return "BLACK";
-    } 
-    else {
-        return "UNKNOWN";
+int getColorReading(int s2State, int s3State) {
+    digitalWrite(S2, s2State);
+    digitalWrite(S3, s3State);
+    delay(100);  // Allow sensor to settle
+    return pulseIn(sensorOut, LOW);  // Measure pulse duration
+}
+
+void pickUpFlag() {
+    myServo.write(100);  //open
+    delay(2000);  // wait 2 seconds to line up flag
+
+    myServo.write(150);  //close, will hold flag
+}
+
+void dropFlag() {
+    myServo.write(100); //opnes claws
+}
+
+//Starter 
+void startUp() {
+
+    red = getColorReading(LOW, LOW); // Read Red
+    green = getColorReading(HIGH, HIGH); // Read Green
+    blue = getColorReading(LOW, HIGH); // Read Blue
+
+    String color = identifyColor(red, green, blue);
+
+    while(color != "BLACK") {
+        moveForward();
+        startTime = millis();
+        timerRunning = true;
+        Serial.println("Timer Started!");
     }
+
+    stopTime = millis();
+    timerRunning = false;
+    longerRuntime = stopTime - startTime;
+
+    turnLeft();
+    String newColor = identifyColor(red, green, blue);
+
+    while(newColor == color || newColor == "BLACK") {
+        turnLeft();
+        delay(1000);
+    }
+    //we wanna keep spinning until we find a color that isnt the same as the ring we are on or black
+    //same logic as the loop function
+    stopMotors();
+}
