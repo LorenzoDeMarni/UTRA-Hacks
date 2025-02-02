d84fa09ef739478355373e13ca99fc86464a154c
adelynntran <161761042+adelynntran@users.noreply.github.com>
2025-02-01 21:17:48 -0500

    add time based tracking for challenge3

--- challenge2/challenge2.ino
+++ challenge2/challenge2.ino
@@ -1,9 +1,55 @@
-const int motor1Pin1 = 9;  // Motor 1 forward
-const int motor1Pin2 = 10; // Motor 1 backward
-const int motor2Pin1 = 5;  // Motor 2 forward
-const int motor2Pin2 = 6;  // Motor 2 backward
+// Motor Driver Pin Definitions
+#define EN_A 11  // PWM speed control for right
+#define motor1Pin1 6   // Control pin for second motor
+#define motor1Pin2 7   // Control pin for second motor
+//left motor
+#define motor2Pin1 8   // Control pin for first motor
+#define motor2Pin2 9   // Control pin for first motor
+//right method
+#define EN_B 10  // PWM speed control for left
+
+// Motor speed (adjustable between 0-255)
+int motorSpeed = 150;  // 150 is about 60% speed
+
+# https://www.instructables.com/Running-DC-Motor-With-Arduino-Using-L298N-Motor-Dr/
+
+// change the pin values for light sensor
+#define S0 2;
+#define S1 3;
+#define S2 4;
+#define S3 5;
+#define sensorOut 8;
+
+int red = 0, green = 0, blue = 0;
 
 void setup() {
+    // Set motor control pins as outputs
+    pinMode(EN_A, OUTPUT);
+    pinMode(motor1Pin1, OUTPUT);
+    pinMode(motor1Pin2, OUTPUT);
+    pinMode(motor2Pin1, OUTPUT);
+    pinMode(motor2Pin2, OUTPUT);
+    pinMode(EN_B, OUTPUT);
+
+    Serial.begin(9600);
+    Serial.println("Motor System Initialized");
+    
+}
+
+void color_setup() {
+    pinMode(S0, OUTPUT);
+    pinMode(S1, OUTPUT);
+    digitalWrite(S0, HIGH);
+    digitalWrite(S1, LOW);
+
+    pinMode(S2, OUTPUT);
+    pinMode(S3, OUTPUT);
+    pinMode(sensorOut, INPUT);
+
+    Serial.begin(9600);
+}
+
+void motor_setup() {
     pinMode(motor1Pin1, OUTPUT);
     pinMode(motor1Pin2, OUTPUT);
     pinMode(motor2Pin1, OUTPUT);
@@ -12,6 +58,25 @@ void setup() {
 }
 
 void loop() {
+    // Move forward for 2 seconds
+    moveForward(2000);
+    // Turn left for 1 second
+    turnLeft(1000);
+    // Move forward for 2 seconds
+    moveForward(2000);
+    // Turn right for 1 second
+    turnRight(1000);
+    // Move forward for 2 seconds
+    moveForward(2000);
+    // Stop motors for 1 second
+    stopMotors();
+    delay(1000);
+    break;
+
+}
+
+
+void base_move() {
     if (Serial.available() > 0) {
         char command = Serial.read();
         if (command == 'F') { // Move Forward
@@ -27,3 +92,123 @@ void loop() {
         }
     }
 }
+
+void moveForward(int duration) {
+    Serial.println("Moving Forward...");
+    analogWrite(EN_A, motorSpeed); // Set speed
+    analogWrite(EN_B, motorSpeed);
+    digitalWrite(motor1Pin1, HIGH);
+    digitalWrite(motor1Pin2, LOW);
+    digitalWrite(motor2Pin1, HIGH);
+    digitalWrite(motor2Pin2, LOW);
+    delay(duration);
+    stopMotors();
+}
+
+void turnLeft(int duration) {
+    Serial.println("Turning Left...");
+    analogWrite(EN_A, motorSpeed); // Set speed
+    analogWrite(EN_B, motorSpeed);
+    digitalWrite(motor1Pin1, LOW);
+    digitalWrite(motor1Pin2, HIGH);
+    digitalWrite(motor2Pin1, HIGH);
+    digitalWrite(motor2Pin2, LOW);
+    delay(duration);
+    stopMotors();
+}
+
+void turnRight(int duration) {
+    Serial.println("Turning Right...");
+    analogWrite(EN_A, motorSpeed); // Set speed
+    analogWrite(EN_B, motorSpeed);
+    digitalWrite(motor1Pin1, HIGH);
+    digitalWrite(motor1Pin2, LOW);
+    digitalWrite(motor2Pin1, LOW);
+    digitalWrite(motor2Pin2, HIGH);
+    delay(duration);
+    stopMotors();
+}
+
+void stopMotors() {
+    Serial.println("Stopping Motors...");
+    digitalWrite(motor1Pin1, LOW);
+    digitalWrite(motor1Pin2, LOW);
+    digitalWrite(motor2Pin1, LOW);
+    digitalWrite(motor2Pin2, LOW);
+}
+
+String get_color() {
+
+    // Read Red
+    digitalWrite(S2, LOW);
+    digitalWrite(S3, LOW);
+    red = pulseIn(sensorOut, LOW);
+
+    // Read Green
+    digitalWrite(S2, HIGH);
+    digitalWrite(S3, HIGH);
+    green = pulseIn(sensorOut, LOW);
+
+    // Read Blue
+    digitalWrite(S2, LOW);
+    digitalWrite(S3, HIGH);
+    blue = pulseIn(sensorOut, LOW);
+
+    Serial.print("Red: ");
+    Serial.print(red);
+    Serial.print("  Green: ");
+    Serial.print(green);
+    Serial.print("  Blue: ");
+    Serial.println(blue);
+
+    String color = identifyColor(red, green, blue);
+    return color;
+
+    delay(200);
+
+}
+
+String identifyColor(int r, int g, int b) {
+    if (r < g && r < b) {
+        return "RED";
+    } 
+    else if (g < r && g < b) {
+        return "GREEN";
+    } 
+    else if (b < r && b < g) {
+        return "BLUE";
+    } 
+    else if (r > 200 && g > 200 && b > 200) {
+        return "WHITE";
+    } 
+    else if (r < 50 && g < 50 && b < 50) {
+        return "BLACK";
+    } 
+    else {
+        return "UNKNOWN";
+    }
+
+    boolean detect_wall() {
+        /*
+        wall_distance = 5 cm;
+        while (distance == false) {
+            moveForward(); 
+        }
+        else {
+            stopMotors();
+            identifyColor();
+            if (color == 'BLUE') {
+                turnLeft(); 
+            }
+            else if (color == 'GREEN') {
+                turnRight();
+            }
+            else if (color == 'RED') {
+                //write U-TURN method
+            }
+            else if (color == 'BLACK') {
+                moveForward();
+            }
+         }
+        */
+    }
--- challenge3/challenge3.ino
+++ challenge3/challenge3.ino
@@ -1,110 +1,205 @@
-const int EN_A = 11; //Enable pin for first motor
-const int IN1 = 9; //control pin for first motor
-const int IN2 = 8; //control pin for first motor
-const int IN3 = 7; //control pin for second motor
-const int IN4 = 6; //control pin for second motor
-const int EN_B = 10; //Enable pin for second motor
-# https://www.instructables.com/Running-DC-Motor-With-Arduino-Using-L298N-Motor-Dr/
+// ========== MOTOR DRIVER PIN DEFINITIONS ==========
+#define EN_A 11
+#define motor1Pin1 6
+#define motor1Pin2 7
+#define motor2Pin1 8
+#define motor2Pin2 9
+#define EN_B 10
 
-// change the pin values for light sensor
-#define S0 2;
-#define S1 3;
-#define S2 4;
-#define S3 5;
-#define sensorOut 8;
+// ========== COLOR SENSOR PIN DEFINITIONS ==========
+#define S0 2
+#define S1 3
+#define S2 5
+#define S3 4
+#define sensorOut 12
 
+// ========== ULTRASONIC SENSOR ==========
+#define TRIG_PIN A4
+#define ECHO_PIN A5
+#define WALL_DISTANCE_THRESHOLD 200  // 20 cm
+
+// ========== GLOBAL VARIABLES ==========
+int motorSpeed = 150;  
 int red = 0, green = 0, blue = 0;
+float estimatedSpeed = 5.0; // Assume robot moves ~5 cm per second
+float estimatedDistance = 0;
+unsigned long lastMoveTime = 0;
+
+// Time-based region tracking
+unsigned long lastGreenExitTime = 0;
+unsigned long lastBlueExitTime = 0;
+bool firstGreenDetected = false;
+bool firstBlueDetected = false;
+
+// Color sequence
+const char* colorSequence[] = {"Red", "Green", "Blue", "Green", "Blue"};
+int currentColorIndex = 0;
 
 void setup() {
+    // Motor setup
+    pinMode(EN_A, OUTPUT);
     pinMode(motor1Pin1, OUTPUT);
     pinMode(motor1Pin2, OUTPUT);
     pinMode(motor2Pin1, OUTPUT);
     pinMode(motor2Pin2, OUTPUT);
-    Serial.begin(9600);
-}
+    pinMode(EN_B, OUTPUT);
 
-void color_setup() {
+    // Color sensor setup
     pinMode(S0, OUTPUT);
     pinMode(S1, OUTPUT);
-    digitalWrite(S0, HIGH);
-    digitalWrite(S1, LOW);
-
     pinMode(S2, OUTPUT);
     pinMode(S3, OUTPUT);
     pinMode(sensorOut, INPUT);
 
+    // Ultrasonic sensor setup
+    pinMode(TRIG_PIN, OUTPUT);
+    pinMode(ECHO_PIN, INPUT);
+
     Serial.begin(9600);
+    Serial.println("System Ready.");
+
+    // Color sensor settings
+    digitalWrite(S0, HIGH);
+    digitalWrite(S1, LOW);
+
+    lastMoveTime = millis();
 }
 
 void loop() {
-    if (Serial.available() > 0) {
-        char command = Serial.read();
-        if (command == 'F') { // Move Forward
-            digitalWrite(motor1Pin1, HIGH);
-            digitalWrite(motor1Pin2, LOW);
-            digitalWrite(motor2Pin1, HIGH);
-            digitalWrite(motor2Pin2, LOW);
-        } else if (command == 'S') { // Stop
-            digitalWrite(motor1Pin1, LOW);
-            digitalWrite(motor1Pin2, LOW);
-            digitalWrite(motor2Pin1, LOW);
-            digitalWrite(motor2Pin2, LOW);
+    moveForward();
+    checkColorSequence();
+    avoidWalls();
+    delay(100);
+}
+
+// ========== MOVEMENT & DISTANCE TRACKING ==========
+void moveForward() {
+    unsigned long currentTime = millis();
+    float moveTime = (currentTime - lastMoveTime) / 1000.0; // Time in seconds
+    estimatedDistance += moveTime * estimatedSpeed;
+    lastMoveTime = currentTime;
+
+    Serial.print("Estimated Distance: ");
+    Serial.print(estimatedDistance);
+    Serial.println(" cm");
+
+    analogWrite(EN_A, motorSpeed);
+    analogWrite(EN_B, motorSpeed);
+    digitalWrite(motor1Pin1, HIGH);
+    digitalWrite(motor1Pin2, LOW);
+    digitalWrite(motor2Pin1, HIGH);
+    digitalWrite(motor2Pin2, LOW);
+}
+
+void stopMotors() {
+    digitalWrite(motor1Pin1, LOW);
+    digitalWrite(motor1Pin2, LOW);
+    digitalWrite(motor2Pin1, LOW);
+    digitalWrite(motor2Pin2, LOW);
+}
+
+// ========== COLOR DETECTION & REGION TRACKING ==========
+void checkColorSequence() {
+    String detectedColor = get_color();
+    unsigned long currentTime = millis();
+
+    if (detectedColor == "Red") return;  // Always ignore after first Red
+
+    if (detectedColor == "Green") {
+        if (!firstGreenDetected) {  
+            firstGreenDetected = true;
+            lastGreenExitTime = currentTime;
+        } else {
+            unsigned long timeSinceLastGreen = currentTime - lastGreenExitTime;
+            float distanceSinceLastGreen = estimatedDistance;
+
+            if (timeSinceLastGreen > 5000 && distanceSinceLastGreen > 20) {  
+                digitalWrite(LED_PIN, HIGH);
+                delay(500);
+                digitalWrite(LED_PIN, LOW);
+                lastGreenExitTime = currentTime;
+                estimatedDistance = 0;  
+                currentColorIndex++;
+            }
+        }
+    } 
+
+    if (detectedColor == "Blue") {
+        if (!firstBlueDetected) {  
+            firstBlueDetected = true;
+            lastBlueExitTime = currentTime;
+        } else {
+            unsigned long timeSinceLastBlue = currentTime - lastBlueExitTime;
+            float distanceSinceLastBlue = estimatedDistance;
+
+            if (timeSinceLastBlue > 5000 && distanceSinceLastBlue > 20) {  
+                digitalWrite(LED_PIN, HIGH);
+                delay(500);
+                digitalWrite(LED_PIN, LOW);
+                lastBlueExitTime = currentTime;
+                estimatedDistance = 0;  
+                currentColorIndex++;
+            }
         }
     }
 }
 
+// ========== COLOR SENSOR FUNCTIONS ==========
 String get_color() {
+    red = getColorReading(LOW, LOW);
+    green = getColorReading(HIGH, HIGH);
+    blue = getColorReading(LOW, HIGH);
 
-    // Read Red
-    digitalWrite(S2, LOW);
-    digitalWrite(S3, LOW);
-    red = pulseIn(sensorOut, LOW);
+    return identifyColor(red, green, blue);
+}
 
-    // Read Green
-    digitalWrite(S2, HIGH);
-    digitalWrite(S3, HIGH);
-    green = pulseIn(sensorOut, LOW);
-
-    // Read Blue
-    digitalWrite(S2, LOW);
-    digitalWrite(S3, HIGH);
-    blue = pulseIn(sensorOut, LOW);
-
-    Serial.print("  Red: ");
-    Serial.print(red);
-    Serial.print("  Green: ");
-    Serial.print(green);
-    Serial.print("  Blue: ");
-    Serial.println(blue);
-
-    String color = identifyColor(red, green, blue);
-    return color;
-
-    delay(200);
-
+int getColorReading(int s2State, int s3State) {
+    digitalWrite(S2, s2State);
+    digitalWrite(S3, s3State);
+    delay(100);
+    return pulseIn(sensorOut, LOW);
 }
 
 String identifyColor(int r, int g, int b) {
-    if (r < g && r < b) {
-        return "RED";
-    } 
-    else if (g < r && g < b) {
-        return "GREEN";
-    } 
-    else if (b < r && b < g) {
-        return "BLUE";
-    } 
-    else if (r > 200 && g > 200 && b > 200) {
-        return "WHITE";
-    } 
-    else if (r < 50 && g < 50 && b < 50) {
-        return "BLACK";
-    } 
-    else {
-        return "UNKNOWN";
+    if (r < g - 15 && r < b - 15) return "RED";
+    else if (g < r - 15 && g < b - 15) return "GREEN";
+    else if (b < r - 15 && b < g - 15) return "BLUE";
+    else return "BLACK";
+}
+
+// ========== ULTRASONIC SENSOR (OBSTACLE AVOIDANCE) ==========
+void avoidWalls() {
+    long duration;
+    float distance = 0.0;
+
+    digitalWrite(TRIG_PIN, LOW);
+    delayMicroseconds(2);
+    digitalWrite(TRIG_PIN, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(TRIG_PIN, LOW);
+
+    duration = pulseIn(ECHO_PIN, HIGH);
+    distance = (duration * 0.343) / 2; 
+
+    Serial.print("Distance to Wall: ");
+    Serial.print(distance);
+    Serial.println(" mm");
+
+    if (distance > 0 && distance <= WALL_DISTANCE_THRESHOLD) {
+       Serial.println("STOP! Obstacle ahead!");
+       stopMotors();
+       delay(500);
+       turnRight(1000);
     }
+}
 
-
-
-
-
+void turnRight(int duration) {
+    analogWrite(EN_A, motorSpeed);
+    analogWrite(EN_B, motorSpeed);
+    digitalWrite(motor1Pin1, HIGH);
+    digitalWrite(motor1Pin2, LOW);
+    digitalWrite(motor2Pin1, LOW);
+    digitalWrite(motor2Pin2, HIGH);
+    delay(duration);
+    stopMotors();
+}
